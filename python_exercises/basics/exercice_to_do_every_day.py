6ccc2efa23b1dc8944b8ac908b47b81944d72c96
TexasThug <leffejeff@gmail.com>
2025-11-18 15:25:14 +0100

    Add daily todo exercise

---
+++ python_exercises/basics/exercice_to_do_every_day.py
@@ -0,0 +1,35 @@
+fruits = ["Pomme", "Kiwi", "Poire", "Banane", "Goyave", "Prune", "Fraise", "Framboise", "Myrtille", "Figue", "Mangue", "Fruit de la Passion"]
+# Fonctions Python de base 
+# Imprime
+print(fruits)
+# Type d'une variable
+print(type(fruits))
+    # Nombre d'éléments
+print(len(fruits))
+    # Demander une saisie utilisateur
+input("yo >")
+    # Créer une suite de nombres 
+print(range(1,5))
+    # Méthode de liste
+    # Ajoute un élément
+fruits.append("Poire")
+    # Retire un élément 
+fruits.remove("Kiwi")
+    # Supprime le dernier élément 
+fruits.pop()
+    # Trier la liste
+fruits.sort()
+    # Trouver la position d'un élément 
+"python".find("t")
+    # Chaine de caractères
+"python"
+    # Met en majuscule
+print("python".upper())
+    # Remplace un texte 
+print("python".replace("py","my"))
+    # Découpe en liste 
+print("py,thon".split(","))
+    # Fusionne une liste en texte 
+print(",".join(['py', 'thon']))
+    # Supprime les espaces début ou fin 
+"  python  ".strip()
