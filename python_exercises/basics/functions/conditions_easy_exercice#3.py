4d379bbe50a59742f71c9fcb7d15534948f0ef38
TexasThug <leffejeff@gmail.com>
2025-11-28 14:34:04 +0100

    Add exercise #3 for conditions and functions

---
+++ python_exercises/basics/functions/conditions_easy_exercice#3.py
@@ -0,0 +1,11 @@
+def even_or_odd(n):
+    if n % 2 == 0 :
+        return "even"
+    else: 
+        return "odd"
+    
+print(even_or_odd(4))
+print(even_or_odd(14.5))
+print(even_or_odd(0))
+print(even_or_odd(1))
+print(even_or_odd(151))
